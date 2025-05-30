a632379fbf35997062596c4c53a6922382f323e9
Huan Zhou <me@per1cycle.org>
2025-05-30 23:11:50 +0800

    upload

---
+++ sgemm/kernels/v1.h
@@ -0,0 +1,19 @@
+/**
+ * In the naive implementation, the grid has 2 dim, and the block also have two dim
+ */
+template<
+	const uint BM,
+	const uint BN, 
+	const uint BK,
+>
+__global__ void v1(uint M, uint N, uint K, half *a, half *b, half *c)
+{
+	int tx = threadIdx.x, ty = threadIdx.y;
+	int x = tx + blockIdx.x * BM;
+	int y = ty + blockIdx.y * BN; // in y dim the step is BN, aka blockDim.y.
+	
+	float tmp = 0.0f;
+	for(int i = 0; i < K; i ++)
+		tmp += (a[x * K + i]) * (b[i * N + y]);
+	c[x * N + y] = (tmp);
+}
